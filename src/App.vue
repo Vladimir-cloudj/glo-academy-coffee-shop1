c2740a6e943e3de8e56bf4c284dad892ff64dafc
Vladimir-cloudj <good-speedF1@yandex.ru>
2026-07-13 19:39:53 +0400

    feat: lesson 2 practice done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="wrapper">
     <hero-view/>
-    <!-- <first-component/> -->
     <footer-component/>
   </div>
 </template>
 
 <script>
-// import FirstComponent  from './components/FirstComponent.vue'
+
 import HeroView from '@/views/HeroView.vue'
 import FooterComponent from "@/components/FooterComponent.vue"
-// import FirstComponent from '@/components/FirstComponent.vue'
 
 export default {
   components:{
-    // FirstComponent,
     HeroView,
     FooterComponent,
   }
@@ -22,9 +19,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  div {
-    span {
-      color:green;
-    }
-  }
+
 </style>
--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -47,27 +47,23 @@
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
                     <div class="best__wrapper">
-                        <div class="best__item">
-                            <img src="@/assets/img/coffee-1.jpg" alt="coffee">
-                            <div class="best__item-title">
-                                Solimo Coffee Beans 2kg
-                            </div>
-                            <div class="best__item-price">10.73$</div>
-                        </div>
-                        <div class="best__item">
+                        <product-card />
+                        <product-card />
+                        <product-card />
+                        <!-- <div class="best__item">
                             <img src="@/assets/img/coffee-2.jpg" alt="coffee">
                             <div class="best__item-title">
                                 Presto Coffee Beans 1kg
                             </div>
                             <div class="best__item-price">15.99$</div>
-                        </div>
-                        <div class="best__item">
+                        </div> -->
+                        <!-- <div class="best__item">
                             <img src="@/assets/img/coffee-3.jpg" alt="coffee">
                             <div class="best__item-title">
                                 AROMISTICO Coffee 1kg
                             </div>
                             <div class="best__item-price">6.99$</div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -79,10 +75,13 @@
 
 <script>
 import NavBarComponent from '@/components/NavBarComponent.vue'
+import ProductCard from '@/components/ProductCard.vue'
+
 
 export default {
     components: {
-        NavBarComponent
+        NavBarComponent,
+        ProductCard,
     }
 }
 </script>
